a2bb55a086ba3152eab95a54cd35dd646ac1a422
Ansh-Singla-1251 <anshsingla990@gmail.com>
2026-08-02 11:35:39 +0000

    C program

---
+++ Week4/bang.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int bang(int x) {
+  return ((~x & ~(~x + 1)) >> 31) & 1;
+}
+int main(int argc, char const *argv[])
+{
+  printf("%d\n", bang(3));   // 0
+  printf("%d\n", bang(0));   // 1
+  printf("%d\n", bang(-5));  // 0
+  return 0;
+}
