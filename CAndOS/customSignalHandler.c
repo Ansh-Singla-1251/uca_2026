b285740ed87a418159e60860f01b9f9e5a268230
Ansh-Singla-1251 <anshsingla990@gmail.com>
2026-09-21 18:01:51 +0530

    Update customSignalHandler.c

--- CAndOS/customSignalHandler.c
+++ CAndOS/customSignalHandler.c
@@ -4,27 +4,22 @@
 #include <unistd.h>
 
 volatile sig_atomic_t signal_count = 0;
-
 void handle_sigint(int sig) {
     signal_count++;
-
-    if (signal_count < 3) {
-        printf("\nSIGINT received! Press Ctrl+C %d more time(s) to exit.\n",
-               3 - signal_count);
-    } else {
+    if(signal_count<3){
+        printf("\nSIGINT received! Press Ctrl+C %d more time(s) to exit.\n",3 - signal_count);
+    }
+    else{
         printf("\nSIGINT received 3 times. Exiting program...\n");
         exit(0);
     }
 }
 
-int main() {
+int main(){
     signal(SIGINT, handle_sigint);
-
-    printf("Program running (PID: %d). Try pressing Ctrl+C...\n", getpid());
-
+    printf("Program running (PID: %d).\n", getpid());
     while (1) {
         sleep(1);
     }
-
     return 0;
 }
